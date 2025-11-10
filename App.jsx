8bfc8b1b74e3a929744647688c851fde84e05f98
Garima Sharma <168468707+GarimaSharma1808@users.noreply.github.com>
2025-11-10 20:33:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
+ import ScanScreen from './src/screens/ScanScreen';
+
+const App = () => {
+  return <HomeScreen />; 
+   return < ScanScreen/>
+};
+
+export default App;
+
+
+
+
